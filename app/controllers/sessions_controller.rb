2264175f1eca7356cc514ec5adf30082a6ef8d53
becs <becs@storypark.com>
2018-12-15 12:41:48 +1300

    Sessions logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      redirect_to @user
+      log_in user
+      redirect_to user
     else
       # Create an error message.
       render 'new'
