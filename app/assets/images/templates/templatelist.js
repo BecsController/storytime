5173c1adcb314aece52482a1df69ba3452fc1a03
becs <becs@storypark.com>
2018-12-14 13:56:17 +1300

    Images added

---
+++ app/assets/images/templates/templatelist.js
@@ -0,0 +1,44 @@
+// Faces
+const facelist = [
+  ('http://localhost:3000/images/templates/faces/1.png'),
+  ('http://localhost:3000/images/templates/faces/2.png'),
+  ('http://localhost:3000/images/templates/faces/3.png'),
+  ('http://localhost:3000/images/templates/faces/4.png'),
+  ('http://localhost:3000/images/templates/faces/5.png'),
+  ('http://localhost:3000/images/templates/faces/6.png'),
+]
+
+// Eyes
+const eyeslist = [
+  ('http://localhost:3000/images/templates/eyes/1.png'),
+  ('http://localhost:3000/images/templates/eyes/2.png'),
+  ('http://localhost:3000/images/templates/eyes/3.png'),
+  ('http://localhost:3000/images/templates/eyes/4.png'),
+  ('http://localhost:3000/images/templates/eyes/5.png'),
+]
+
+// HairStyles
+const hairlist = [
+  ('http://localhost:3000/images/templates/hair/1.png'),
+  ('http://localhost:3000/images/templates/hair/2.png'),
+  ('http://localhost:3000/images/templates/hair/3.png'),
+  ('http://localhost:3000/images/templates/hair/4.png'),
+  ('http://localhost:3000/images/templates/hair/5.png'),
+  ('http://localhost:3000/images/templates/hair/6.png'),
+  ('http://localhost:3000/images/templates/hair/7.png'),
+  ('http://localhost:3000/images/templates/hair/8.png'),
+  ('http://localhost:3000/images/templates/hair/9.png'),
+  ('http://localhost:3000/images/templates/hair/10.png'),
+  ('http://localhost:3000/images/templates/hair/11.png'),
+  ('http://localhost:3000/images/templates/hair/12.png'),
+  ('http://localhost:3000/images/templates/hair/13.png'),
+  ('http://localhost:3000/images/templates/hair/14.png'),
+  ('http://localhost:3000/images/templates/hair/15.png'),
+  ('http://localhost:3000/images/templates/hair/16.png'),
+]
+
+export {
+  hairlist,
+  facelist,
+  eyeslist
+}
