d04ba3844f847fc35b70c576a87ec2e3b2ad892f
becs <becs@storypark.com>
2018-12-14 15:35:25 +1300

    Linting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal:true
+
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  validates :first_name,  presence: true, length: { maximum: 50 }
+  validates :first_name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
