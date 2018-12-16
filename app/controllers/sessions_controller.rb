857deadaef2ccae6b6e44bbe6c86154e1de643f8
becs <becs@storypark.com>
2018-12-17 08:44:38 +1300

    Fix variables for sessions yet again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @ser && @user.authenticate(params[:session][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash[:danger] = 'Invalid email/password combination'
