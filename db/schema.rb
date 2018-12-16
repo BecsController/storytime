16375f7f0ce80422715c39cb20bea1dd625d0ba4
becs <becs@storypark.com>
2018-12-16 13:38:22 +1300

    Setting up remembering users sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181214014429) do
+ActiveRecord::Schema.define(version: 20181216002111) do
 
   create_table "stories", force: :cascade do |t|
     t.string "title"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20181214014429) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
